ca210113dd7705ee65c067046d6e1b6b5bfe3fac
olyanemyrovska <110451749+olyanemyrovska@users.noreply.github.com>
2024-07-07 11:08:51 +0200

    Added handling of edge cases for Strongest robot calculation

--- Homework_7_Java_Operators_Conditions/JavaArraysAndLoops/src/StrongestRobot.java
+++ Homework_7_Java_Operators_Conditions/JavaArraysAndLoops/src/StrongestRobot.java
@@ -1,16 +1,25 @@
 public class StrongestRobot {
     public static void main(String[] args) {
         int[] weights = {300, 450, 200, 500, 350, 900, 0};
+       // int[] weights = {12};
 
-        int strongestRobot = -1;
-        int maxWeight = 0;//assuming that min weight is 0
+        if (weights.length == 0) {
+            System.out.println("There is no robots in current category");
+        } else if (weights.length == 1) {
+            System.out.println("There is only one robot in this category. Please add more than one to find the strongest one.");
+        } else {
+            int strongestRobot = -1;
+            int maxWeight = 0;//assuming that min weight is 0
 
-        for (int i = 0; i < weights.length; i++) {
-            if (weights[i] > maxWeight) {
-                maxWeight = weights[i];
-                strongestRobot = i;
+            for (int i = 0; i < weights.length; i++) {
+                if (weights[i] > maxWeight) {
+                    maxWeight = weights[i];
+                    strongestRobot = i;
+                }
             }
+
+            System.out.println("Strongest robot number is: " + strongestRobot + " " +
+                    "and he (it?) lifted: " + weights[strongestRobot] + " kg.");
         }
-        System.out.println("Strongest robot number is: " + strongestRobot);
     }
 }
